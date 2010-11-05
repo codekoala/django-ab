552a8e3e5c80c34f98d2e1d75805d08e13a7839d
Josh VanderLinden <codekoala@gmail.com>
2010-11-04 21:04:07 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ab',
-    version='0.1',
+    version='0.2',
     description='A simple AB Testing app for Django!',
     author='John Boxall',
     author_email='john@handimobility.ca',
